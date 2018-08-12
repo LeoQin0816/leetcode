4004710af546b94190b5cd1ae6fbb65b1989b71f
LeoQinl <706046320@qq.com>
2018-08-12 09:54:34 +0800

    538. Convert BST to Greater Tree

---
+++ Algorithms/Java/Tree/538.ConvertBSTToGreaterTree.java
@@ -0,0 +1,66 @@
+package Tree;
+
+/*
+538. Convert BST to Greater Tree
+
+Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
+
+Example:
+
+Input: The root of a Binary Search Tree like this:
+              5
+            /   \
+           2     13
+
+Output: The root of a Greater Tree like this:
+             18
+            /   \
+          20     13
+ */
+
+/*
+538. 把二叉搜索树转换为累加树
+
+给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater Tree)，使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+
+例如：
+
+输入: 二叉搜索树:
+              5
+            /   \
+           2     13
+
+输出: 转换为累加树:
+             18
+            /   \
+          20     13
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution538 {
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) return null;
+        convertBST(root.right);
+        root.val += sum;
+        sum = root.val;
+        convertBST(root.left);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = Wrapper.stringToTreeNode("[5,2,13]");
+        Wrapper.prettyPrintTree(treeNode);
+        TreeNode root = new Solution538().convertBST(treeNode);
+        Wrapper.prettyPrintTree(root);
+    }
+}
