6e83317c7b56410cdbaaf567e3efdc728a294f99
LeoQinl <706046320@qq.com>
2018-08-15 10:42:58 +0800

    21. Merge Two Sorted Lists;

---
+++ Algorithms/Java/LinkedList/21.MergeTwoSortedLists.java
@@ -0,0 +1,55 @@
+package LinkedList;
+
+/*
+21. Merge Two Sorted Lists
+
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+ */
+
+/*
+21. 合并两个有序链表
+
+
+将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+示例：
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = Wrapper.stringToListNode("[1,2,4]");
+        Wrapper.prettyPrintLinkedList(l1);
+        ListNode l2 = Wrapper.stringToListNode("[1,3,4]");
+        Wrapper.prettyPrintLinkedList(l2);
+        ListNode node = new Solution21().mergeTwoLists(l1, l2);
+        Wrapper.prettyPrintLinkedList(node);
+    }
+}
