8c5cb959795560995f0d9842f06d35e4b170a082
LeoQinl <706046320@qq.com>
2018-08-26 10:35:34 +0800

    add python

---
+++ Algorithms/python/Tree/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/24 下午6:20
+# @Author  : LeoQin
+# @Site    : 
+# @File    : test.py
+# @Software: PyCharm
+
+import TreeNode;
+
+l = TreeNode.stringToTreeNode("[1,2,3,4,null,5]")
+TreeNode.prettyPrintTree(l)
