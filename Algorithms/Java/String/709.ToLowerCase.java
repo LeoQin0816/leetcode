36dab3635da5221117c2a3e88f8f460c903a20f7
LeoQinl <706046320@qq.com>
2018-09-06 10:39:48 +0800

    709. To Lower Case

---
+++ Algorithms/Java/String/709.ToLowerCase.java
@@ -0,0 +1,59 @@
+package String;
+
+/*
+709. To Lower Case
+
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+
+Example 2:
+
+Input: "here"
+Output: "here"
+
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+ */
+
+/*
+709. 转换成小写字母
+
+实现函数 ToLowerCase()，该函数接收一个字符串参数 str，并将该字符串中的大写字母转换成小写字母，之后返回新的字符串。
+
+示例 1：
+
+输入: "Hello"
+输出: "hello"
+
+示例 2：
+
+输入: "here"
+输出: "here"
+
+示例 3：
+
+输入: "LOVELY"
+输出: "lovely"
+ */
+
+class Solution709 {
+    public String toLowerCase(String str) {
+        char[] a = str.toCharArray();
+        for (int i = 0; i < a.length; i++)
+            if ('A' <= a[i] && a[i] <= 'Z')
+                a[i] = (char) (a[i] - 'A' + 'a');
+        return new String(a);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution709().toLowerCase("Hello"));
+        System.out.println(new Solution709().toLowerCase("here"));
+        System.out.println(new Solution709().toLowerCase("LOVELY"));
+    }
+}
