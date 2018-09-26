a094cdfcab2c7078ea3b4910f6ea41d563b2f936
LeoQinl <706046320@qq.com>
2018-09-26 11:19:30 +0800

    204. Count Primes

--- Algorithms/Java/204.CountPrimes.java
+++ Algorithms/Java/204.CountPrimes.java
@@ -22,8 +22,23 @@ Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 解释: 小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
  */
 
-//class Solution204 {
-//    public int countPrimes(int n) {
-//
-//    }
-//}
+class Solution204 {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i] == false) {
+                count++;
+                for (int j = 2; i * j < n; j++) {
+                    notPrime[i * j] = true;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution204().countPrimes(10));
+    }
+}
