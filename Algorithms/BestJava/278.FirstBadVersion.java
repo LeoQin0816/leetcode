8fa888c001a21258898a44abb3c57ad6da779a88
LeoQinl <706046320@qq.com>
2019-02-18 14:42:01 +0800

    278. First Bad Version

--- Algorithms/BestJava/278.FirstBadVersion.java
+++ Algorithms/BestJava/278.FirstBadVersion.java
@@ -23,7 +23,7 @@ public class Solution278 extends VersionControl {
     public int firstBadVersion(int n) {
         int start = 1, end = n;
         while (start < end) {
-            int mid = (end + start) / 2;
+            int mid = start + (end - start) / 2;
             if (!isBadVersion(mid)) start = mid + 1;
             else end = mid;
         }
