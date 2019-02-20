d033ceb6ebbeeb29525354fb6c6be8d3ada08ae7
LeoQinl <706046320@qq.com>
2019-02-20 11:18:00 +0800

    107. Binary Tree Level Order Traversal II

--- Algorithms/BestJava/107.BinaryTreeLevelOrderTraversalII.java
+++ Algorithms/BestJava/107.BinaryTreeLevelOrderTraversalII.java
@@ -16,19 +16,43 @@ return its bottom-up level order traversal as:
 ]
  */
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
  * }
  */
-//class Solution {
-//    public List<List<Integer>> levelOrderBottom(TreeNode root) {
-//
-//    }
-//}
+class Solution107 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) return result;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (q.size() > 0) {
+            List<Integer> list = new ArrayList<>();
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                list.add(node.val);
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+            result.add(0, list);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = TreeWrapper.stringToTreeNode("[3,9,20,null,null,15,7]");
+        TreeWrapper.prettyPrintTree(node);
+        System.out.println(new Solution107().levelOrderBottom(node));
+    }
+}
