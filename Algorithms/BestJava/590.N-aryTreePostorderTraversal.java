aa433e9d6656b97f6e0b0346c0439877cb173f12
LeoQinl <706046320@qq.com>
2019-03-01 10:29:00 +0800

    590

---
+++ Algorithms/BestJava/590.N-aryTreePostorderTraversal.java
@@ -0,0 +1,75 @@
+/*
+Given an n-ary tree, return the postorder traversal of its nodes' values.
+
+For example, given a 3-ary tree:
+
+
+
+
+
+
+
+Return its postorder traversal as: [5,6,3,2,4,1].
+
+
+Note:
+
+Recursive solution is trivial, could you do it iteratively?
+ */
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution590 {
+    public List<Integer> postorder(Node root) {
+        LinkedList<Integer> list = new LinkedList<>();
+        if (root == null) return list;
+
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            Node temp = stack.pop();
+            list.addFirst(temp.val);
+            for (Node n : temp.children) {
+                stack.push(n);
+            }
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Node subSubChild1 = new Node(5, new LinkedList<>());
+        Node subSubChild2 = new Node(6, new LinkedList<>());
+        List<Node> subSubChild = new LinkedList<>();
+        subSubChild.add(subSubChild1);
+        subSubChild.add(subSubChild2);
+        Node subChild1 = new Node(3, subSubChild);
+        Node subChild2 = new Node(2, new LinkedList<>());
+        Node subChild3 = new Node(4, new LinkedList<>());
+        List<Node> subChild = new LinkedList<>();
+        subChild.add(subChild1);
+        subChild.add(subChild2);
+        subChild.add(subChild3);
+        Node root = new Node(1, subChild);
+
+        System.out.println(new Solution590().postorder(root));
+    }
+}
