b1d515af0a3e2d15bc9ee147d4211db935a33e7c
LeoQinl <706046320@qq.com>
2019-03-04 11:47:04 +0800

    1. 两数之和

--- Algorithms/Java/MyJava/1.TwoSum.java
+++ Algorithms/Java/MyJava/1.TwoSum.java
@@ -12,8 +12,24 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
  */
 
-//class Solution1 {
-//    public int[] twoSum(int[] nums, int target) {
-//
-//    }
-//}
+class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int[] res = new Solution1().twoSum(nums, 9);
+        System.out.printf("%d  %d", res[0], res[1]);
+    }
+}
