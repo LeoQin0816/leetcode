63b94288c129e138db55a210b42cdee907bbc141
LeoQinl <706046320@qq.com>
2019-03-05 09:48:10 +0800

    1. 两数之和

--- Algorithms/Java/MyJava/1.TwoSum.java
+++ Algorithms/Java/MyJava/1.TwoSum.java
@@ -16,7 +16,7 @@ class Solution1 {
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
                     res[0] = i;
                     res[1] = j;
