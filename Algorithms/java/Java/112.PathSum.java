35dfeca6e47d580355f5cccb66b3be222504a914
LeoQinl <706046320@qq.com>
2019-03-14 10:25:51 +0800

    112. 路径总和

---
+++ Algorithms/java/Java/112.PathSum.java
@@ -0,0 +1,45 @@
+package Java;
+/*
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given the below binary tree and sum = 22,
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \      \
+7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+ */
+
+import Utils.TreeNode.TreeNode;
+import Utils.TreeNode.TreeWrapper;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution112 {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        if (sum - root.val == 0 && root.left == null && root.right == null) return true;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = TreeWrapper.stringToTreeNode("[5,4,8,11,null,13,4,7,2,null,null,null,1]");
+        TreeWrapper.prettyPrintTree(treeNode);
+        System.out.println(new Solution112().hasPathSum(treeNode, 22));
+    }
+}
