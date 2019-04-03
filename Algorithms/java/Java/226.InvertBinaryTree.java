f045e5b001ad7a43f6277daae1a3d9ab7095abfa
LeoQinl <706046320@qq.com>
2019-04-03 10:30:16 +0800

    226. 翻转二叉树

---
+++ Algorithms/java/Java/226.InvertBinaryTree.java
@@ -0,0 +1,54 @@
+package Java;
+/*
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+ */
+
+import Utils.TreeNode.TreeNode;
+import Utils.TreeNode.TreeWrapper;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = invertTree(right);
+        root.right = invertTree(left);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = TreeWrapper.stringToTreeNode("[4,2,7,1,3,6,9]");
+        TreeWrapper.prettyPrintTree(treeNode);
+        TreeWrapper.prettyPrintTree(new Solution226().invertTree(treeNode));
+    }
+}
