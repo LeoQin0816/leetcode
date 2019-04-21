be59fa404948198d01a44720edb4579947cf2668
LeoQinl <706046320@qq.com>
2019-04-21 22:27:22 +0800

    326. 3的幂
    326. 3的幂

---
+++ Algorithms/java/Java/326.PowerOfThree.java
@@ -0,0 +1,36 @@
+package Java;
+/*
+Given an integer, write a function to determine if it is a power of three.
+
+Example 1:
+
+Input: 27
+Output: true
+Example 2:
+
+Input: 0
+Output: false
+Example 3:
+
+Input: 9
+Output: true
+Example 4:
+
+Input: 45
+Output: false
+Follow up:
+Could you do it without using any loop / recursion?
+ */
+
+class Solution326 {
+    public boolean isPowerOfThree(int n) {
+        return n >= 0 && Math.pow(3, 19) % n == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution326().isPowerOfThree(27));
+        System.out.println(new Solution326().isPowerOfThree(0));
+        System.out.println(new Solution326().isPowerOfThree(9));
+        System.out.println(new Solution326().isPowerOfThree(45));
+    }
+}
