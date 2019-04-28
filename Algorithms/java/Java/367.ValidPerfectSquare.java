a010d68d294517f4d0326f36e2c637b0772d2aad
LeoQinl <706046320@qq.com>
2019-04-28 10:03:41 +0800

    367. Valid Perfect Square

---
+++ Algorithms/java/Java/367.ValidPerfectSquare.java
@@ -0,0 +1,29 @@
+package Java;
+/*
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Note: Do not use any built-in library function such as sqrt.
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 14
+Output: false
+ */
+
+class Solution367 {
+    public boolean isPerfectSquare(int num) {
+        if (num <= 1) return true;
+        long left = 0, right = num / 2;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            if (mid * mid == num) return true;
+            if (mid * mid < num) left = mid + 1;
+            if (mid * mid > num) right = mid - 1;
+        }
+        return false;
+    }
+}
