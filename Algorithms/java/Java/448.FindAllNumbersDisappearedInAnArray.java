e11408b3eb4e445d8b5127163ddbb7301ce39571
LeoQinl <706046320@qq.com>
2019-05-24 10:28:54 +0800

    448. 找到所有数组中消失的数字

--- Algorithms/java/Java/448.FindAllNumbersDisappearedInAnArray.java
+++ Algorithms/java/Java/448.FindAllNumbersDisappearedInAnArray.java
@@ -1,12 +1,13 @@
-package BestJava;
+package Java;
 /*
-448. Find All Numbers Disappeared in an Array
-
 Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
 Find all the elements of [1, n] inclusive that do not appear in this array.
+
 Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
 
 Example:
+
 Input:
 [4,3,2,7,8,2,3,1]
 
@@ -19,25 +20,20 @@ import java.util.List;
 
 class Solution448 {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> ret = new ArrayList<>();
-
+        List<Integer> res = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             int val = Math.abs(nums[i]) - 1;
-            if (nums[val] > 0) {
-                nums[val] = -nums[val];
-            }
+            nums[val] = -Math.abs(nums[val]);
         }
-
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > 0) {
-                ret.add(i + 1);
+                res.add(i + 1);
             }
         }
-        return ret;
+        return res;
     }
 
     public static void main(String[] args) {
-        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
-        System.out.println(new Solution448().findDisappearedNumbers(nums));
+        System.out.println(new Solution448().findDisappearedNumbers(new int[]{4, 3, 2, 7, 8, 2, 3, 1}));
     }
 }
