38dc5778b23d122343a4c5c446798fc95b3b856a
LeoQinl <706046320@qq.com>
2019-09-06 09:57:56 +0800

    palindrome-number

--- Java/Algorithms/session1/9.palindrome-number.java
+++ Java/Algorithms/session1/9.palindrome-number.java
@@ -46,9 +46,17 @@
  * Coud you solve it without converting the integer to a string?
  * 
  */
-class Solution {
+package session1;
+
+class Solution9 {
     public boolean isPalindrome(int x) {
-        
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return (x == rev || rev / 10 == x);
     }
 }
-
