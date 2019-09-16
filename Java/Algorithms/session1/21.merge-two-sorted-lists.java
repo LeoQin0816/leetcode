e698a6160339552a4cb9ec5b68b35b4bb8677d7e
LeoQinl <706046320@qq.com>
2019-09-16 09:52:17 +0800

    [21] Merge Two Sorted Lists

--- Java/Algorithms/session1/21.merge-two-sorted-lists.java
+++ Java/Algorithms/session1/21.merge-two-sorted-lists.java
@@ -31,9 +31,26 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+package session1;
+
+import Utils.ListNode.*;
+
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        
+        ListNode res = new ListNode(0);
+        ListNode dym = res;
+        while (l1 != null && l2 != null) {
+            if (l1.val >= l2.val) {
+                dym.next = l2;
+                l2 = l2.next;
+            } else {
+                dym.next = l1;
+                l1 = l1.next;
+            }
+            dym = dym.next;
+        }
+        dym.next = l1 == null ? l2 : l1;
+        return res.next;
     }
-}
 
+}
