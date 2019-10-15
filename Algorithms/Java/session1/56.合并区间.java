0350c581e990b974b129fa93577d93a597a07a07
LeoQinl <706046320@qq.com>
2019-10-15 10:07:43 +0800

    合并区间

---
+++ Algorithms/Java/session1/56.合并区间.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=56 lang=java
+ *
+ * [56] 合并区间
+ *
+ * https://leetcode-cn.com/problems/merge-intervals/description/
+ *
+ * algorithms
+ * Medium (38.76%)
+ * Likes:    196
+ * Dislikes: 0
+ * Total Accepted:    32.1K
+ * Total Submissions: 82.7K
+ * Testcase Example:  '[[1,3],[2,6],[8,10],[15,18]]'
+ *
+ * 给出一个区间的集合，请合并所有重叠的区间。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [[1,3],[2,6],[8,10],[15,18]]
+ * 输出: [[1,6],[8,10],[15,18]]
+ * 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [[1,4],[4,5]]
+ * 输出: [[1,5]]
+ * 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> res = new ArrayList<>();
+        if (intervals == null || intervals.length == 0) {
+            return res.toArray(new int[0][0]);
+        }
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        for (int i = 0; i < intervals.length; i++) {
+            int left = intervals[i][0];
+            int right = intervals[i][1];
+            while (i < intervals.length - 1 && intervals[i + 1][0] <= right) {
+                right = Math.max(intervals[++i][1], right);
+            }
+            res.add(new int[] { left, right });
+        }
+        return res.toArray(new int[0][0]);
+    }
+}
+// @lc code=end
