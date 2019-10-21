dd7f721426b7889ec1fbc4fce9952b4b19b2db08
LeoQinl <706046320@qq.com>
2019-10-21 11:33:18 +0800

    78. 子集

---
+++ Algorithms/Java/session1/78.子集.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=78 lang=java
+ *
+ * [78] 子集
+ *
+ * https://leetcode-cn.com/problems/subsets/description/
+ *
+ * algorithms
+ * Medium (74.98%)
+ * Likes:    365
+ * Dislikes: 0
+ * Total Accepted:    41.1K
+ * Total Submissions: 54.8K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ * 
+ * 说明：解集不能包含重复的子集。
+ * 
+ * 示例:
+ * 
+ * 输入: nums = [1,2,3]
+ * 输出:
+ * [
+ * ⁠ [3],
+ * [1],
+ * [2],
+ * [1,2,3],
+ * [1,3],
+ * [2,3],
+ * [1,2],
+ * []
+ * ]
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(0, nums, res, new ArrayList<>());
+        return res;
+    }
+
+    private void backtrack(int i, int[] nums, List<List<Integer>> res, ArrayList<Integer> temp) {
+        res.add(new ArrayList<>(temp));
+        for (int j = i; j < nums.length; j++) {
+            temp.add(nums[j]);
+            backtrack(j + 1, nums, res, temp);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
+// @lc code=end
