3718752308db1e98827b5b091b9b3e67da54389c
LeoQinl <706046320@qq.com>
2019-10-22 09:54:58 +0800

    79.单词搜索

---
+++ Algorithms/Java/session1/79.单词搜索.java
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=79 lang=java
+ *
+ * [79] 单词搜索
+ *
+ * https://leetcode-cn.com/problems/word-search/description/
+ *
+ * algorithms
+ * Medium (39.15%)
+ * Likes:    226
+ * Dislikes: 0
+ * Total Accepted:    23K
+ * Total Submissions: 58.6K
+ * Testcase Example:  '[["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]\n"ABCCED"'
+ *
+ * 给定一个二维网格和一个单词，找出该单词是否存在于网格中。
+ * 
+ * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+ * 
+ * 示例:
+ * 
+ * board =
+ * [
+ * ⁠ ['A','B','C','E'],
+ * ⁠ ['S','F','C','S'],
+ * ⁠ ['A','D','E','E']
+ * ]
+ * 
+ * 给定 word = "ABCCED", 返回 true.
+ * 给定 word = "SEE", 返回 true.
+ * 给定 word = "ABCB", 返回 false.
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (search(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean search(char[][] board, String word, int i, int j, int k) {
+        if (k >= word.length())
+            return true;
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(k))
+            return false;
+        board[i][j] += 256;
+        boolean result = search(board, word, i - 1, j, k + 1) || search(board, word, i + 1, j, k + 1)
+                || search(board, word, i, j - 1, k + 1) || search(board, word, i, j + 1, k + 1);
+        board[i][j] -= 256;
+        return result;
+    }
+}
+// @lc code=end
