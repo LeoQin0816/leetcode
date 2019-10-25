4ddebdb6e2e169f6640343d032ad3feaa1fd682d
LeoQinl <706046320@qq.com>
2019-10-25 09:34:58 +0800

    三角形最小路径和

---
+++ Algorithms/Java/session1/119.杨辉三角-ii.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=119 lang=java
+ *
+ * [119] 杨辉三角 II
+ *
+ * https://leetcode-cn.com/problems/pascals-triangle-ii/description/
+ *
+ * algorithms
+ * Easy (58.26%)
+ * Likes:    88
+ * Dislikes: 0
+ * Total Accepted:    27K
+ * Total Submissions: 46.1K
+ * Testcase Example:  '3'
+ *
+ * 给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
+ * 
+ * 
+ * 
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ * 
+ * 示例:
+ * 
+ * 输入: 3
+ * 输出: [1,3,3,1]
+ * 
+ * 
+ * 进阶：
+ * 
+ * 你可以优化你的算法到 O(k) 空间复杂度吗？
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            res.add(0, 1);
+            for (int j = 1; j < res.size() - 1; j++) {
+                res.set(j, res.get(j) + res.get(j + 1));
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
---
+++ Algorithms/Java/session1/120.三角形最小路径和.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=120 lang=java
+ *
+ * [120] 三角形最小路径和
+ *
+ * https://leetcode-cn.com/problems/triangle/description/
+ *
+ * algorithms
+ * Medium (62.07%)
+ * Likes:    237
+ * Dislikes: 0
+ * Total Accepted:    26.3K
+ * Total Submissions: 42.2K
+ * Testcase Example:  '[[2],[3,4],[6,5,7],[4,1,8,3]]'
+ *
+ * 给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+ * 
+ * 例如，给定三角形：
+ * 
+ * [
+ * ⁠    [2],
+ * ⁠   [3,4],
+ * ⁠  [6,5,7],
+ * ⁠ [4,1,8,3]
+ * ]
+ * 
+ * 
+ * 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+ * 
+ * 说明：
+ * 
+ * 如果你可以只使用 O(n) 的额外空间（n 为三角形的总行数）来解决这个问题，那么你的算法会很加分。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                triangle.get(i).set(j,
+                        triangle.get(i).get(j) + Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1)));
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}
+// @lc code=end
