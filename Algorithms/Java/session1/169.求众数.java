fb93bd4688d1dfc595f1b0e6d1b9a48fac8a8226
LeoQinl <706046320@qq.com>
2019-11-01 09:43:48 +0800

    169.求众数

---
+++ Algorithms/Java/session1/169.求众数.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode.cn id=169 lang=java
+ *
+ * [169] 求众数
+ *
+ * https://leetcode-cn.com/problems/majority-element/description/
+ *
+ * algorithms
+ * Easy (60.35%)
+ * Likes:    352
+ * Dislikes: 0
+ * Total Accepted:    81.3K
+ * Total Submissions: 134.5K
+ * Testcase Example:  '[3,2,3]'
+ *
+ * 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ * 
+ * 你可以假设数组是非空的，并且给定的数组总是存在众数。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [3,2,3]
+ * 输出: 3
+ * 
+ * 示例 2:
+ * 
+ * 输入: [2,2,1,1,1,2,2]
+ * 输出: 2
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        int res = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > nums.length / 2) {
+                res = entry.getKey();
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
