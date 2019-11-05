b329a5298602ccd6e164c322323faba567a93baa
LeoQinl <706046320@qq.com>
2019-11-05 11:57:22 +0800

    长度最小的子数组

---
+++ Algorithms/Java/session1/209.长度最小的子数组.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=209 lang=java
+ *
+ * [209] 长度最小的子数组
+ *
+ * https://leetcode-cn.com/problems/minimum-size-subarray-sum/description/
+ *
+ * algorithms
+ * Medium (39.90%)
+ * Likes:    143
+ * Dislikes: 0
+ * Total Accepted:    18.6K
+ * Total Submissions: 46.4K
+ * Testcase Example:  '7\n[2,3,1,2,4,3]'
+ *
+ * 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的连续子数组。如果不存在符合条件的连续子数组，返回 0。
+ * 
+ * 示例: 
+ * 
+ * 输入: s = 7, nums = [2,3,1,2,4,3]
+ * 输出: 2
+ * 解释: 子数组 [4,3] 是该条件下的长度最小的连续子数组。
+ * 
+ * 
+ * 进阶:
+ * 
+ * 如果你已经完成了O(n) 时间复杂度的解法, 请尝试 O(n log n) 时间复杂度的解法。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        int left = 0, right = 0;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
+        while (right < nums.length) {
+            if (sum + nums[right] < s) {
+                sum += nums[right++];
+            } else {
+                minLength = Math.min(minLength, right - left + 1);
+                sum -= nums[left++];
+            }
+        }
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+}
+// @lc code=end
